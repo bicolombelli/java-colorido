55738ddce382afcbd6e30ba2082f02ae817fb4fc
Bianca Colombelli <110998923+bicolombelli@users.noreply.github.com>
2024-10-24 14:45:26 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800,800);
+
+  background(200);
+
+  let c1 = color("rgb(143,73,73)");
+
+  fill(c1);
+  noStroke();
+  circle(750, 705, 800);
+
+  let c2 = color("rgb(228,189,18)9");
+
+  fill(c2);
+  circle(205, 250, 800);
+}
